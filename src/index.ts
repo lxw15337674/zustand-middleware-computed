d77b602fd3ff3d136b9319990ddea3b6b037b0e4
bhwa233 <404174262@qq.com>
2023-09-09 20:30:04 +0800

    回滚代码

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,40 @@
-import { memoize } from 'proxy-memoize';
-import { StateCreator } from 'zustand';
+import { memoize } from 'proxy-memoize'
+import { StateCreator } from 'zustand'
 
-type ComputeObj<T, A> = {
-  [K in keyof A]: (state: T & A) => A[K];
-};
-
-function shallowClone<T extends Record<string, any>>(obj: T): T {
-  return Object.create(
-    Object.getPrototypeOf(obj),
-    Object.getOwnPropertyDescriptors(obj),
-  );
-}
+type ComputeObj<T, A> = Record<keyof A, (state: T & A) => A[keyof A]>
+type Computed<T, A> = { [K in keyof A]: ReturnType<ComputeObj<T, A>[K]> }
 
 const computed = <T extends object, A extends object>(
-  f: StateCreator<T>,
-  compute: ComputeObj<T, A>,
-): StateCreator<T & A, [], []> => {
-  return (set, get, api) => {
-    const computedState = Object.entries(compute).reduce(
-      (acc, [key, value]) => {
-        const memoizeComputeFn = memoize(value as (state: T) => A[keyof A]);
-        Object.defineProperty(acc, key, {
-          get() {
-            return memoizeComputeFn(this);
-          },
-        });
-        return acc;
-      },
-      {} as T & A,
-    );
-
-    const withComputed = (state: T) =>
-      Object.assign(shallowClone(computedState), state);
-
-    let lastState: T & A;
-    let isChanged = true;
-    api.getState = () => {
-      if (!isChanged) {
-        return lastState;
-      }
-      lastState = withComputed(get());
-      isChanged = false;
-      return lastState;
-    };
-    api.setState = (state, replace) => {
-        set(state, replace)
-        isChanged = true
+    f: StateCreator<T, [], []>,
+    compute: ComputeObj<T, A>
+): StateCreator<T & Computed<T, A>, [], []> => {
+    return (set, get, api) => {
+        type Store = ReturnType<typeof f>
+        const computedList = Object.entries(compute).map(([key, value]) => {
+            return [key, memoize(value as (state: T) => A[keyof A])] as [keyof A, (state: T) => A[keyof A]]
+        })
+        const computeAndMerge = (state: T) => {
+            return computedList.reduce(
+                (acc, [key, value]) => ({
+                    ...acc,
+                    [key]: value({ ...state, ...acc }),
+                }),
+                { ...state }
+            ) as Store & Computed<T, A>
+        }
+        const setWithComputed = (update: T | ((state: T) => T), replace?: boolean) => {
+            set((state: T) => {
+                const updated = typeof update === 'function' ? update(state) : update
+                return computeAndMerge({
+                    ...state,
+                    ...updated,
+                })
+            }, replace)
+        }
+        api.setState = setWithComputed
+        const st = f(setWithComputed, get, api)
+        return computeAndMerge(st)
     }
-    const { subscribe } = api;
-    api.subscribe = (listener) => {
-      return subscribe((state, prevState) => {
-        listener(
-          withComputed(state),
-          withComputed(prevState),
-        );
-      });
-    }
-    const st = f(api.setState, get, api);
-    return st as T & A;
-  };
-};
-
+}
 
 export default computed
